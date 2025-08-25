4c92724c6451fea8d591e2fe5b68623eb7b509d4
João Marcos Cavalcanti <joaomarcosnog10@gmail.com>
2025-08-25 20:54:09 -0300

    criação da classe Processo, com seus atributos e construtor

---
+++ src/Processo.java
@@ -0,0 +1,18 @@
+public class Processo {
+     int id;
+     String nome;
+     int prioridade;
+     int ciclo_necessarios;
+     String recurso_necessario;
+
+public Processo (int id,String nome, int prioridade, int ciclos_necessarios,String recurso_necessario){
+    this.id=id;
+    this.nome=nome;
+     this.prioridade=prioridade;
+     this.ciclo_necessarios=ciclos_necessarios;
+     this.recurso_necessario=recurso_necessario;
+
+}
+
+
+}
