27455a807327011fad75fa7e53c50ffe0c110ded
Eduardo <eduardooliveira7907@gmail.com>
2025-08-28 20:54:52 -0300

    adicionando os seguintes métodos na classe lista de processos: getTamanho , VerInicio e estaVazia

--- src/Listadeprocessos.java
+++ src/Listadeprocessos.java
@@ -17,25 +17,38 @@ public class Listadeprocessos {
         } else {
             this.fim.proximoNo = novoNo;
             this.fim = novoNo;
-
         }
         this.tamanho++;
-
     }
 
     public Processo removerDoInicio() {
         if (this.inicio == null) {
             throw new RuntimeException("fila vazia!");
         }
-
-    Processo p= this.inicio.processo;
-    this.inicio= this.inicio.proximoNo;
-   if(this.inicio== null){
-       this.fim=null;
-   }
-this.tamanho--;
-   return p;
+        Processo p = this.inicio.processo;
+        this.inicio = this.inicio.proximoNo;
+        if (this.inicio == null) {
+            this.fim = null;
+        }
+        this.tamanho--;
+        return p;
     }
 
+    public int getTamanho() {
+        return this.tamanho;
     }
 
+    public boolean estaVazia() {
+        if (this.inicio == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        public Processo verInicio () {
+            if (estaVazia()) {
+                return null;
+            }
+            return this.inicio.processo;
+        }
+    }
